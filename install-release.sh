7d2c083710858629a643a40b7f81b51209116780
githubphone <sifangdazhai@gmail.com>
2019-06-11 02:10:00 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -155,7 +155,7 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     colorEcho ${BLUE} "Downloading V2Ray."
-    DOWNLOAD_LINK="https://github.com/v2ray/v2ray-core/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+    DOWNLOAD_LINK="https://github.com/v2ray/v2ray-core/releases/download/v4.19.1/v2ray-linux-64.zip"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
